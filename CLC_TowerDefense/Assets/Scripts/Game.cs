efaae59740e3156b02a6315a9bb3f08dfa4a7877
HwangHyunJing <bbq2277@naver.com>
2021-08-17 16:33:46 +0900

    6.1: 1.2까지 함

---
+++ CLC_TowerDefense/Assets/Scripts/Game.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    // 게임 보드의 전체 크기
+    [SerializeField]
+    Vector2Int boardSize = new Vector2Int(11, 11);
+
+    // 게임 보드의 정보를 받아옴
+    [SerializeField]
+    GameBoard board = default;
+
+    private void Awake()
+    {
+        // Game Board의 크기를 초기화
+        board.Initialize(boardSize);
+    }
+
+    private void OnValidate()
+    {
+        if(boardSize.x < 2)
+        {
+            boardSize.x = 2;
+        }
+        if(boardSize.y < 2)
+        {
+            boardSize.y = 2;
+        }
+    }
+}
