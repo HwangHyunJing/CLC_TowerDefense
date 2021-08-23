10cb9ae5646b3ae52d534c2b064336bb22b652b4
HwangHyunJing <bbq2277@naver.com>
2021-08-23 12:45:20 +0900

    6.1: 2.1 - 근데 뭔지 이해가 1도 안감. 심지어 졸려서 더더욱...

--- CLC_TowerDefense/Assets/Scripts/GameTile.cs
+++ CLC_TowerDefense/Assets/Scripts/GameTile.cs
@@ -6,4 +6,27 @@ public class GameTile : MonoBehaviour
 {
     [SerializeField]
     Transform arrow = default;
+
+    GameTile north, east, south, west;
+
+    public static void MakeEastWestNeighbors(GameTile east, GameTile west)
+    {
+        // 첫 인자가 true인지 확인
+        Debug.Assert(
+            west.east == null && east.west == null, "Refined neighbors!");
+
+        // 자신의 서쪽 타일의 동쪽 성분에, ....??
+        west.east = east;
+        east.west = west;
+    }
+
+    public static void MakeNorthSouthNeighbors(GameTile north, GameTile south)
+    {
+        Debug.Assert(
+            south.north == null && north.south == null, "Refined neighbors!");
+
+        //
+        south.north = north;
+        north.south = south;
+    }
 }
